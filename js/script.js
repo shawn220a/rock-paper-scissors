41027148be18ffc7deb515cc2dddd8f2e244187f
shawn220a <shawn.tschoepe@icloud.com>
2019-12-29 18:52:03 -0600

    Add js to update html on user and computer choices

--- js/script.js
+++ js/script.js
@@ -10,14 +10,22 @@ var yWin = winsScore;
 var yLose = losesScore;
 var yTies = tiesScore;
 
+var yPick = document.getElementsByClassName("yourPick");
+var cPick = document.getElementsByClassName("comPick");
+
+var yours = yPick[0].innerText;
+var comps = cPick[0].innerText;
+
 var userChoice = "";
 var compChoices = ["r", "p", "s"];
 
 // Records users choice for Rock button
 function rock() {
   userChoice = "r";
+  yPick[0].innerText = userChoice;
   var comp = Math.floor(Math.random() * compChoices.length);
   var cChoice = compChoices[comp];
+  cPick[0].innerText = cChoice;
   if (cChoice === "s") {
     win();
   } else if (cChoice === "p") { 
@@ -30,8 +38,10 @@ function rock() {
 // Records users choice for Paper button
 function paper() {
   userChoice = "p";
+  yPick[0].innerText = userChoice;
   var comp = Math.floor(Math.random() * compChoices.length);
   var cChoice = compChoices[comp];
+  cPick[0].innerText = cChoice;
   if (cChoice === "r") {
     win();
   } else if (cChoice === "s") {   
@@ -44,8 +54,10 @@ function paper() {
 // Records users choice for Scissors button
 function scissors() {
   userChoice = "s";
+  yPick[0].innerText = userChoice;
   var comp = Math.floor(Math.random() * compChoices.length);
   var cChoice = compChoices[comp];
+  cPick[0].innerText = cChoice;
   if (cChoice === "p") {
     win();
   } else if (cChoice === "r") { 
